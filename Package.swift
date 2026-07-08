7057114ec08d1fd4e9084d02bb700f6e5732db7b
Siuzanna Karagulova <siuzannakaragulova@gmail.com>
2026-07-08 20:46:47 +0600

    refs #S-923 Lower VersionTracker iOS deployment target to 15

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "VersionTracker",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(name: "VersionTracker", targets: ["VersionTracker"]),
         .library(name: "VersionTrackerUI", targets: ["VersionTrackerUI"])
